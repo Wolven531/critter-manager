97b2fdde76698d57f8da1043796fc850a6b3c41b
Anthony Williams <anthony.williams.cs@gmail.com>
2019-09-12 21:14:46 -0600

    Convert CritterList to not use react-hooks

--- src/views/CritterList/CritterList.tsx
+++ src/views/CritterList/CritterList.tsx
@@ -1,11 +1,11 @@
-import React, { FC, useEffect } from 'react'
+import React, { Component } from 'react'
 
 import { Critter } from '../../components/Critter/Critter'
 import { CritterListControls } from '../../components/CritterListControls/CritterListControls'
 
 import { COMBAT_COST } from '../../service/combatService'
 
-import { useCritterState } from '../../state/useCritterState'
+import { Critter as CritterModel } from '../../model/Critter'
 
 import './CritterList.scss'
 
@@ -13,27 +13,74 @@ export interface ICritterListProps {
 	currentMoney: number
 }
 
-const CritterList: FC<ICritterListProps> = ({ currentMoney }) => {
-	const { clearCritters, critters, loadFromStorage, saveToLocalStorage, spawnCritter } = useCritterState([])
+class CritterList extends Component<ICritterListProps, {}> {
+	private critterState: any
 
-	useEffect(() => {
-		loadFromStorage()
-	}, [])
+	constructor(props: ICritterListProps) {
+		super(props)
+		this.critterState = new CritterState([])
+	}
 
-	return (
-		<article className="critter-list">
-			<CritterListControls
-				canStartCombat={currentMoney >= COMBAT_COST}
-				clearCritters={clearCritters}
-				saveToLocalStorage={saveToLocalStorage}
-				shouldShowCombat={true}
-				spawnCritter={spawnCritter}
-				startCombat={() => {}} />
-			<section className="display-container">
-				{critters.map(critter => <Critter key={critter.id} critter={critter} />)}
-			</section>
-		</article>
-	)
+	public componentDidMount() {
+		this.critterState.loadFromStorage()
+	}
+
+	public render() {
+		if (!this.critterState) {
+			return null
+		}
+
+		const { clearCritters, critters, saveToLocalStorage, spawnCritter } = this.critterState
+
+		return (
+			<article className="critter-list">
+				<CritterListControls
+					canStartCombat={this.critterState.currentMoney >= COMBAT_COST}
+					clearCritters={clearCritters}
+					saveToLocalStorage={saveToLocalStorage}
+					shouldShowCombat={true}
+					spawnCritter={spawnCritter}
+					startCombat={() => {}} />
+				<section className="display-container">
+					{critters.map((critter: CritterModel) => <Critter key={critter.id} critter={critter} />)}
+				</section>
+			</article>
+		)
+	}
 }
+
+////// NOTE: attempt w/ react hooks, useEffect
+
+// import React, { FC, useEffect } from 'react'
+
+// import { Critter } from '../../components/Critter/Critter'
+// import { CritterListControls } from '../../components/CritterListControls/CritterListControls'
+
+// import { COMBAT_COST } from '../../service/combatService'
+
+// import { useCritterState } from '../../state/useCritterState'
+
+// const CritterList: FC<ICritterListProps> = ({ currentMoney }) => {
+// 	const { clearCritters, critters, loadFromStorage, saveToLocalStorage, spawnCritter } = useCritterState([])
+
+// 	useEffect(() => {
+// 		loadFromStorage()
+// 	}, [])
+
+// 	return (
+// 		<article className="critter-list">
+// 			<CritterListControls
+// 				canStartCombat={currentMoney >= COMBAT_COST}
+// 				clearCritters={clearCritters}
+// 				saveToLocalStorage={saveToLocalStorage}
+// 				shouldShowCombat={true}
+// 				spawnCritter={spawnCritter}
+// 				startCombat={() => {}} />
+// 			<section className="display-container">
+// 				{critters.map(critter => <Critter key={critter.id} critter={critter} />)}
+// 			</section>
+// 		</article>
+// 	)
+// }
 
 export { CritterList }
