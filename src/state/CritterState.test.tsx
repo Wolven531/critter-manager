c0dc4778e4224ca9d92f4c71b5a09c6a97fcd8c6
Wolven531 <anthony.williams.cs@gmail.com>
2019-09-13 23:58:41 -0600

    Add unit test for CritterState.addCritter()

--- src/state/CritterState.test.tsx
+++ src/state/CritterState.test.tsx
@@ -5,7 +5,7 @@ import { CritterState } from './CritterState'
 describe('CritterState unit tests', () => {
 	let fixture: CritterState
 
-	describe('constructor behavior', () => {
+	describe('initialize CritterState', () => {
 		beforeEach(() => {
 			fixture = new CritterState(10 * 50, [new CritterModel('critter 1', 10, 1, 0, 'id1')])
 		})
@@ -16,6 +16,19 @@ describe('CritterState unit tests', () => {
 
 		it('should set critters', () => {
 			expect(fixture.critters).toEqual([new CritterModel('critter 1', 10, 1, 0, 'id1')])
+		})
+	})
+
+	describe('invoke addCritter', () => {
+		beforeEach(() => {
+			fixture.addCritter(new CritterModel('critter 2', 5, 5, 2, 'id2'))
+		})
+
+		it('should update critters', () => {
+			expect(fixture.critters).toEqual([
+				new CritterModel('critter 1', 10, 1, 0, 'id1'),
+				new CritterModel('critter 2', 5, 5, 2, 'id2')
+			])
 		})
 	})
 })
