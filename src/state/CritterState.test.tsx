739e21db458570189481c77fbbea34d7a5f93e45
Wolven531 <anthony.williams.cs@gmail.com>
2019-09-13 23:55:56 -0600

    Add unit tests for CritterState constructor

--- src/state/CritterState.test.tsx
+++ src/state/CritterState.test.tsx
@@ -1,5 +1,21 @@
+import { Critter as CritterModel } from '../model/Critter'
+
 import { CritterState } from './CritterState'
 
 describe('CritterState unit tests', () => {
-	
+	let fixture: CritterState
+
+	describe('constructor behavior', () => {
+		beforeEach(() => {
+			fixture = new CritterState(10 * 50, [new CritterModel('critter 1', 10, 1, 0, 'id1')])
+		})
+
+		it('should set currentMoney', () => {
+			expect(fixture.currentMoney).toBe(500)
+		})
+
+		it('should set critters', () => {
+			expect(fixture.critters).toEqual([new CritterModel('critter 1', 10, 1, 0, 'id1')])
+		})
+	})
 })
