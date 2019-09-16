a54625ef53732447a322ee1f3bb46fdc4cbf42c4
Wolven531 <anthony.williams.cs@gmail.com>
2019-09-15 23:34:32 -0600

    Start tests for UpgradeState

---
+++ src/state/UpgradeState/UpgradeState.test.tsx
@@ -0,0 +1,15 @@
+import { UpgradeState } from './UpgradeState'
+
+describe('UpgradeState unit tests', () => {
+	let fixture: UpgradeState
+
+	describe('initialize UpgradeState', () => {
+		beforeEach(() => {
+			fixture = new UpgradeState(3)
+		})
+
+		it('should set gathererLevel', () => {
+			expect(fixture.gathererLevel).toBe(3)
+		})
+	})
+})
