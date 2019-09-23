c9eaa1d028aca674c6e621aecc1d65c8062b0ee8
Wolven531 <anthony.williams.cs@gmail.com>
2019-09-23 00:07:08 -0600

    Add gatherer constants to constants file

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
+export const GATHERER_COST = 10
+export const GATHERER_INCOME = 1
+export const GATHERER_INITIAL_TICK = 0
+export const GATHERER_TICK_RATE = 10
+export const GATHERER_TIME_SECONDS = 2
+
 export const STORAGEKEY_GATHERERS = 'critter-manager.gatherers'
 export const STORAGEKEY_GATHERLEVEL = 'critter-manager.gatherLevel'
 export const STORAGEKEY_MONEY = 'critter-manager.money'
