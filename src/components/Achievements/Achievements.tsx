21c4df95494ef4965455b5245e6fa518e26ad455
Wolven531 <anthony.williams.cs@gmail.com>
2019-09-29 22:05:26 -0600

    Add ability to switch active achievement set

--- src/components/Achievements/Achievements.tsx
+++ src/components/Achievements/Achievements.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { monify } from '../utils'
 
 import './Achievements.scss'
 
 const Achievements = () => {
+	const [activeAchievements, setActiveAchievements] = useState('money-achievements')
+
 	return (
 		<article className="achievements">
 			<section className="wrapper">
@@ -13,19 +15,21 @@ const Achievements = () => {
 				</section>
 				<section className="box sidebar">
 					<ul>
-						<li>Money</li>
-						<li>Gatherers</li>
+						<li onClick={() => setActiveAchievements('money-achievements')}>Money</li>
+						<li onClick={() => setActiveAchievements('gatherer-achievements')}>Gatherers</li>
 					</ul>
 				</section>
 				<section className="box content">
-					<ul className="achievement-list money-achievements active">
+					<ul className={'achievement-list money-achievements'.concat(
+						activeAchievements === 'money-achievements' ? ' active' : '')}>
 						<li>Collect {monify(100)}</li>
 						<li>Collect {monify(1000)}</li>
 						<li>Collect {monify(10000)}</li>
 						<li>Collect {monify(100000)}</li>
 						<li>Collect {monify(1000000)}</li>
 					</ul>
-					<ul className="achievement-list gatherer-achievements">
+					<ul className={'achievement-list gatherer-achievements'.concat(
+						activeAchievements === 'gatherer-achievements' ? ' active' : '')}>
 						<li>Collect 1 gatherer</li>
 						<li>Collect 5 gatherers</li>
 						<li>Collect 10 gatherers</li>
