4cfca983f5c6bf64ed27b3213be48bba9966bd89
Wolven531 <anthony.williams.cs@gmail.com>
2019-09-29 21:49:09 -0600

    Add basic structure to Achievements

--- src/components/Achievements/Achievements.tsx
+++ src/components/Achievements/Achievements.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
+import './Achievements.scss'
+
 const Achievements = () => {
 	return (
-		<article>
-			<h3>Achievements</h3>
+		<article className="achievements">
+			<section className="wrapper">
+				<section className="box header">
+					<h3>Achievements</h3>
+				</section>
+				<section className="box sidebar">
+					<ul>
+						<li>Money</li>
+						<li>Gatherers</li>
+					</ul>
+				</section>
+				<section className="box content">Content</section>
+			</section>
 		</article>
 	)
 }
