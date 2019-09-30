542b66e7c54a62fe9a562ca40e5dd2098c6bd92e
Wolven531 <anthony.williams.cs@gmail.com>
2019-09-29 21:56:39 -0600

    Add first 5 achievements for each category

--- src/components/Achievements/Achievements.tsx
+++ src/components/Achievements/Achievements.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { monify } from '../utils'
+
 import './Achievements.scss'
 
 const Achievements = () => {
@@ -15,7 +17,22 @@ const Achievements = () => {
 						<li>Gatherers</li>
 					</ul>
 				</section>
-				<section className="box content">Content</section>
+				<section className="box content">
+					<ul className="achievement-list money-achievements active">
+						<li>Collect {monify(100)}</li>
+						<li>Collect {monify(1000)}</li>
+						<li>Collect {monify(10000)}</li>
+						<li>Collect {monify(100000)}</li>
+						<li>Collect {monify(1000000)}</li>
+					</ul>
+					<ul className="achievement-list gatherer-achievements">
+						<li>Collect 1 gatherer</li>
+						<li>Collect 5 gatherers</li>
+						<li>Collect 10 gatherers</li>
+						<li>Collect 50 gatherers</li>
+						<li>Collect 100 gatherers</li>
+					</ul>
+				</section>
 			</section>
 		</article>
 	)
